8d4a0df3c4e0acdcaca7ce7ae4d7feafc21a6688
Florent <Florent@Florent-PC.home>
2018-06-28 22:21:28 +0200

    a

--- src/App/Personnage.java
+++ src/App/Personnage.java
@@ -1,9 +1,9 @@
 package App;
 
 public class Personnage {
-	private Object vitesse;
-	private Object qualite;
-	private Object fatigue;
+	private int vitesse;
+	private int qualite;
+	private int fatigue;
 	private String nom;
 
 	public Personnage(String nom,int vitesse, int qualite, int fatigue) {
@@ -12,4 +12,37 @@ public class Personnage {
 		this.qualite = qualite;
 		this.fatigue = fatigue;
 	}
+
+	public Object getVitesse() {
+		return vitesse;
+	}
+
+	public void setVitesse(int vitesse) {
+		this.vitesse = vitesse;
+	}
+
+	public Object getQualite() {
+		return this.qualite;
+	}
+
+	public void setQualite(int qualite) {
+		this.qualite = qualite;
+	}
+
+	public Object getFatigue() {
+		return this.fatigue;
+	}
+
+	public void setFatigue(int fatigue) {
+		this.fatigue = fatigue;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }
